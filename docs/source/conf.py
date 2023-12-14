6f75ac5935c0dc9f09424ed6a3c28cd8515ac168
eric <fantasy_2003_@hotmail.com>
2023-12-15 01:06:31 +0800

    modify v7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,6 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = 'alabaster'
-#html_theme = 'sphinx_rtd_theme'
-html_theme = "classic1"
+html_theme = 'sphinx_rtd_theme'
+#html_theme = "classic1"
 html_static_path = ['_static']
